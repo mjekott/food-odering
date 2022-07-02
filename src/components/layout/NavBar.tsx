61bfd19a63a691d56b60ac73aa82d1c2aca670fa
mjekott <ekottmfon@yahoo.com>
2022-07-02 22:28:53 +0100

    feat: implemented navbar

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 const appLinks = [
   {
-    path: '/',
+    path: '/browse',
     name: 'Browse',
   },
   {
@@ -28,22 +28,22 @@ const NavBar = () => {
 
   return (
     <nav className='border-b border-gray-300'>
-      <div className='flex flex-col lg:flex-row lg:justify-between  lg:items-center container space-y-3 lg:space-y-0 lg:py-3 pb-3 '>
-        <div className='flex justify-between items-center py-3 lg:py-0 '>
-          <div>Order App</div>
+      <div className='container flex flex-col space-y-3  pb-3 lg:flex-row lg:items-center lg:justify-between lg:space-y-0 lg:py-3 '>
+        <div className='flex items-center justify-between py-3 lg:py-0 '>
+          <Link to='/'>Order App</Link>
           <button type='button' className='lg:hidden' onClick={toggleNav}>
-            <MdReorder className='w-6 h-6' />
+            <MdReorder className='h-6 w-6' />
           </button>
         </div>
         <div className={`lg:order-3 ${openMobileNav ? 'block' : 'hidden'} lg:block`}>
           <input
             type='search'
-            className='bg-gray-50 border border-gray-300 w-full rounded outline-none px-5 py-1'
+            className='w-full rounded border border-gray-300 bg-gray-50 px-5 py-1 outline-none'
             placeholder='Search items'
           />
         </div>
         <ul
-          className={`flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-5 ${
+          className={`flex-col space-y-4 lg:flex-row lg:space-y-0 lg:space-x-5 ${
             openMobileNav ? 'flex' : 'hidden'
           } lg:flex`}
         >
